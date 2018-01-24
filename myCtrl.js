90919164376f55a5d477ddef062a7ec6e22bd0a9
Raxotine <32650159+Raxotine@users.noreply.github.com>
2018-01-24 17:27:16 -0600

    Add files via upload

---
+++ myCtrl.js
@@ -0,0 +1,22 @@
+app.controller('myCtrl', function($scope) {
+	$scope.names = ["Eat breakfast", "Brush teeth", "Shower"];
+	$scope.tasks = [false, false];
+	$scope.firstvar = "Add tasks";
+	$scope.class = "undone";
+	$scope.vari = "";
+	$scope.adding = function () {
+		$scope.errortext = "";
+		if (!$scope.newTask) {return;}
+		if ($scope.names.indexOf($scope.newTask) == -1) {
+			$scope.names.push($scope.newTask);
+			$scope.newTask = "";
+		} else {
+
+			$scope.errortext = "You already have that task on the list.";
+		}
+	}
+	$scope.delete = function (x) {
+		$scope.names.splice(x, 1);
+	}
+
+});
